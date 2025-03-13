ea0b31014510484fccd2b73a246a67ce4309fc3e
Caique Matos <caiquematos@pronton.me>
2025-03-13 13:02:46 -0300

    Add  Custom Fonts

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,38 @@
 <template>
-    <header class="flex justify-between items-center p-4 bg-primary">
+    <header class="flex justify-between items-center py-6 px-32 bg-white font-DM-Sans">
+        <!-- Logo -->
         <img src="@/assets/SportNews.svg" alt="Logo">
+
+        <!-- Nav Bar -->
         <nav>
-            <ul class="flex text-[rgba(38,38,38,0.5)] text-base font-normal">
+            <ul class="flex text-[rgba(38,38,38,0.5)] text-base">
                 <li v-for="item in Menu" :key="item.name">
                     <a :href="item.href"
-                        class="block whitespace-nowrap px-4 "
+                        :class="{'font-bold text-black': item.name === 'Home'}" 
+                        class="block whitespace-nowrap px-4"
                     >
                         {{ item.name }}
                     </a>
                 </li>
             </ul>
         </nav>
-        <button class=" rounded-lg w-24 h-9 flex justify-center items-center bg-[#B8C2CE] text-sm">
-            <div>
-                O
-            </div>
-            <div>
-                login
-            </div>
+
+        <!-- Search Button -->
+        <button class="rounded-lg w-24 h-9 bg-[#B8C2CE]">
+            <span class="flex justify-center items-center">
+                <!-- Search Icon -->
+                <Search size="16" color="white" class="mr-1.5"/>
+                <p class="text-white text-sm">Search</p>
+            </span>
         </button>
     </header>
 </template>
 
 <script setup>
+// Lucide Icons
+import { Search } from 'lucide-vue-next';
+
+// Defining the navbar items
 import { ref } from 'vue';
     const Menu =ref([
         {name:'Home' ,href:'#home'},
