4abd0ab4dac6897cfea1e5e8b61563a36a6a239a
Caique Matos <caiquematos@pronton.me>
2025-03-14 11:40:29 -0300

    Fixing Header

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,34 @@
 <template>
-    <header class="flex justify-between items-center py-6 px-38 bg-white font-DM-Sans">
-        <!-- Logo -->
-        <img src="@/assets/sportnews.svg" alt="Logo">
+    <header class="flex bg-white w-full justify-center font-DM-Sans">
 
-        <!-- Nav Bar -->
-        <nav>
-            <ul class="flex text-[rgba(38,38,38,0.5)] text-base">
-                <li v-for="item in Menu" :key="item.name">
-                    <a :href="item.href"
-                        :class="{'font-bold text-black': item.name === 'Home'}" 
-                        class="block whitespace-nowrap px-4"
-                    >
-                        {{ item.name }}
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <div class="flex py-6 justify-between items-center w-[1170px]">
+            <!-- Logo -->
+            <img src="@/assets/sportnews.svg" alt="Logo">
 
-        <!-- Search Button -->
-        <button class="rounded-lg w-24 h-9 bg-[#B8C2CE]">
-            <span class="flex justify-center items-center">
-                <!-- Search Icon -->
-                <Search size="16" color="white" class="mr-1.5"/>
-                <p class="text-white text-sm">Search</p>
-            </span>
-        </button>
+            <!-- Nav Bar -->
+            <nav>
+                <ul class="flex text-[rgba(38,38,38,0.5)] text-base">
+                    <li v-for="item in Menu" :key="item.name">
+                        <a :href="item.href"
+                            :class="{'font-bold text-black': item.name === 'Home'}" 
+                            class="block whitespace-nowrap px-4"
+                        >
+                            {{ item.name }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <!-- Search Button -->
+            <button class="rounded-lg w-24 h-9 bg-[#B8C2CE]">
+                <span class="flex justify-center items-center">
+                    <!-- Search Icon -->
+                    <Search size="16" color="white" class="mr-1.5"/>
+                    <p class="text-white text-sm">Search</p>
+                </span>
+            </button>    
+        </div>
+        
     </header>
 </template>
 
